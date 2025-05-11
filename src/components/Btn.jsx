74375b5a5129f4e6c6cacefe5f32676dc02f7f50
minechoo <choominhae@gamil.com>
2025-05-11 09:36:30 +0900

    삼항연산자

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 export default function Btn({label, handelClick}) {
-  //console.log(props)
+
   const defaultOpt = {label: "button", handelClick: null}
-  if(!label) label = defaultOpt.label
-  if(!handelClick) handelClick = defaultOpt.handelClick
+  // if(!label) label = defaultOpt.label
+  // if(!handelClick) handelClick = defaultOpt.handelClick
   return (
-    <button onClick={handelClick}>{label}</button>
+    <button onClick={handelClick ? handelClick : defaultOpt.handelClick}>{label ? label : defaultOpt.label}</button>
   )
 }
 
